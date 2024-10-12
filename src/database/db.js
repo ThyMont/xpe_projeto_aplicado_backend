ac08acf5b6e30ff30e4e6ad726787159b77b473e
Thyago Monteiro <tmacademico@gmail.com>
2024-10-12 10:08:31 -0300

    Ajuste Banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -8,7 +8,7 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: "postgres",
+  dialect: process.env.DB_DIALECT,
   logging: false, // Você pode habilitar o log se quiser ver as queries SQL
 });
 
