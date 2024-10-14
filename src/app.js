a72804d46d9763d2db7734b7a8bee142fe60102b
Thyago Monteiro <tmacademico@gmail.com>
2024-10-14 16:21:17 -0300

    Arquivo dist

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send({ message: "API - Go Appa" });
 });
-
+app.use(express.static(path.join(__dirname, "dist")));
 app.use("/api-docs", express.static("dist/api-docs"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
